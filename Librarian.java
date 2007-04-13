a9d60ac4704fec36792269844957e4adfa962387
Florent Daignière <nextgens@freenetproject.org>
2007-04-13 19:08:12 +0000

    Librarian: fix it
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Librarian@12652 67a373e5-eb02-0410-a15c-ee090a768436

--- Librarian.java
+++ Librarian.java
@@ -42,10 +42,10 @@ public class Librarian implements FredPlugin, FredPluginHTTP, FredPluginThreadle
 		}
 	}
 	public String handleHTTPPut(HTTPRequest request) throws PluginHTTPException {
-		throw new PluginHTTPException();
+		throw new PluginHTTPException("PUT requests aren't allowed!", "/plugins");
 	}
 	public String handleHTTPPost(HTTPRequest request) throws PluginHTTPException {
-		throw new PluginHTTPException();
+		throw new PluginHTTPException("POST requests aren't allowed!", "/plugins");
 	}
 	
 	private HashMap getElements(String path) {
